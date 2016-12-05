1d3505a9c678c4625631043ecb4c5b2b85d6460d
pawelkowalik55 <pawelkowalik55@gmail.com>
2016-12-05 23:30:45 +0100

    metoda wyswietl

--- Car_Project/Car_Project/Car.cs
+++ Car_Project/Car_Project/Car.cs
@@ -8,5 +8,23 @@ namespace Car_Project
 {
     class Car
     {
+        public string Marka;
+        public string Model;
+        public double PojemnoscSilnika;
+        public int PojemnoscBaku;
+
+        public Car(string marka, string model, int pojemnoscbaku, double pojemnodcsilnika )
+        {
+            Marka = marka;
+            Model = model;
+            PojemnoscSilnika = pojemnodcsilnika;
+            PojemnoscBaku = pojemnoscbaku;
+        }
+
+        public void wyswietl()
+        {
+            Console.WriteLine("Marka: {0}\nModel: {1}\nPojemnosc Silnika: {2}\nPojemnosc Baku: {3}",Marka,Model,PojemnoscSilnika,PojemnoscBaku);
+        }
     }
+
 }
--- Car_Project/Car_Project/Program.cs
+++ Car_Project/Car_Project/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,16 @@ namespace Car_Project
             SredniaPredkosc spala = new SredniaPredkosc();
             spala.trasa = 120;
             spala.czas = 1.5;
+            Car samo = new Car("Lancia ", "Lybra ", 60, 1.8);
+            samo.wyswietl();
+            List<Car> samochód = new List<Car>();
+            samochód.Add(new Car("Lancia ","Lybra ",60,1.8));
+            
+            //Console.WriteLine(samochód);
+            
+            
             spala.sredniapredkosc(spala.trasa, spala.czas);
+            
             Console.WriteLine(CzasPracy.TodayTime.TimeOfDay);
             
 
