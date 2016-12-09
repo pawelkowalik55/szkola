eea01ed463656689ad5c8e816ae507b191b1f1b4
pawelkowalik55 <pawelkowalik55@gmail.com>
2016-12-09 02:41:10 +0100

    dodanie windows forms

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.spd_up = new System.Windows.Forms.Button();
+            this.spd_down = new System.Windows.Forms.Button();
+            this.spd = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.distance = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // spd_up
+            // 
+            this.spd_up.Location = new System.Drawing.Point(154, 35);
+            this.spd_up.Name = "spd_up";
+            this.spd_up.Size = new System.Drawing.Size(118, 23);
+            this.spd_up.TabIndex = 0;
+            this.spd_up.Text = "zwiększ prędkość";
+            this.spd_up.UseVisualStyleBackColor = true;
+            this.spd_up.Click += new System.EventHandler(this.spd_up_Click);
+            // 
+            // spd_down
+            // 
+            this.spd_down.Location = new System.Drawing.Point(154, 65);
+            this.spd_down.Name = "spd_down";
+            this.spd_down.Size = new System.Drawing.Size(118, 23);
+            this.spd_down.TabIndex = 1;
+            this.spd_down.Text = "zmniejsz predkosc";
+            this.spd_down.UseVisualStyleBackColor = true;
+            this.spd_down.Click += new System.EventHandler(this.spd_down_Click);
+            // 
+            // spd
+            // 
+            this.spd.Location = new System.Drawing.Point(6, 19);
+            this.spd.Name = "spd";
+            this.spd.Size = new System.Drawing.Size(100, 20);
+            this.spd.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.spd);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(136, 78);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Prędkość";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.distance);
+            this.groupBox2.Location = new System.Drawing.Point(12, 96);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(136, 75);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Przebyty dystans";
+            // 
+            // distance
+            // 
+            this.distance.Location = new System.Drawing.Point(6, 20);
+            this.distance.Name = "distance";
+            this.distance.Size = new System.Drawing.Size(100, 20);
+            this.distance.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.spd_down);
+            this.Controls.Add(this.spd_up);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button spd_up;
+        private System.Windows.Forms.Button spd_down;
+        private System.Windows.Forms.TextBox spd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox distance;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static Random r = new Random(5);
+        public int speed = r.Next(0, 200);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(TimeSpan.FromSeconds(1));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void spd_up_Click(object sender, EventArgs e)
+        {
+            speed++;
+            spd.Text = Convert.ToString(speed);
+        }
+
+        private void spd_down_Click(object sender, EventArgs e)
+        {
+            speed--;
+            spd.Text = Convert.ToString(speed);
+        }
+    }
+}
